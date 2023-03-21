fed2229d7eaf5bb610d3b66f1cd16ac05281e503
Desire1990 <desnduwim26@gmail.com>
2023-03-21 17:10:35 +0200

    deploy

--- inkokoApi/urls.py
+++ inkokoApi/urls.py
@@ -1,4 +1,4 @@
-# from . import views
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -7,6 +7,6 @@ from django.urls import path, include, re_path
 
 urlpatterns = [
     path('api/', include('api.urls')),
-    # re_path("^.*$", views.index),    
+    re_path("^.*$", views.index),    
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
